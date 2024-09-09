5560815aff1e215191ad90477c18265d18511f9a
Zuzanna Chmielowska <b747.programmer@gmail.com>
2024-09-09 12:46:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask , render_template,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
+
 app = Flask(__name__)
 # Podłączanie SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
@@ -11,10 +12,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Creating a DB
 db = SQLAlchemy(app )
 
+
+
 # Uruchamianie strony z treścią
 @app.route('/')
-def index():\
-    return render_template('index.html')
+def index():
+    # Wyświetlanie obiektów Bazy
+    # Assignment #2. Display the objects from the DB in index.html
+    cards = Card.query.order_by(Card.id).all()
+    
+    return render_template('index.html',
+                           #karty = cards
+                            cards=cards
+                           )
+
+
+# Uruchomienie strony z kartą
+@app.route('/card/<int:id>')
+def card(id):
+    #Zadanie #2. Wyświetl właściwą kartę według jej identyfikatora
+    card = Card.query.get(id)
+
+    return render_template('card.html', card=card)
 
 
 # Umiejętności dynamiczne
@@ -26,11 +45,34 @@ def process_form():
     button_db = request.form.get('button_db')
     return render_template('index.html', button_python=button_python , button_discord=button_discord , button_html=button_html , button_db=button_db)
 
-class form(db.Model):
+
+class Card(db.Model):
    # Tworzenie kolumn
     # Adres mailowy
-    email = db.Column(db.String, primary_key=True, autoincrement=True)
-    text = db.Column(db.String, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, primary_key=True)
+
+    def __repr__(self):
+        return f'<Card {self.id}>'
+
+
+# Formularz zgłoszeniowy
+@app.route('/form_create', methods=['GET','POST'])
+def form_create():
+    if request.method == 'POST':
+        id =  request.form['id']
+        text =  request.form['text']
+
+        # Tworzenie obiektu, który zostanie wysłany do bazy danych
+        card = Card(id=id, text=text)
+
+        db.session.add(card)
+        db.session.commit()
+        return redirect('/index')
+    else:
+        return render_template('create_card.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
